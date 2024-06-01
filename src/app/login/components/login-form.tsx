34d18508569829a7888575f197c008f6ced6615f
Lucas Ribeiro <lucasribeirodesouza@live.com>
2024-06-01 19:07:10 -0300

    style: login form page

--- src/app/login/components/login-form.tsx
+++ src/app/login/components/login-form.tsx
@@ -1,9 +1,11 @@
 'use client'
 
+import styles from './login-form.module.css'
 import login from "@/actions/login";
 import Button from "@/components/button/button";
 import ErrorMessage from "@/components/helper/error-message";
 import Input from "@/components/input/input";
+import Link from "next/link";
 import { useEffect } from "react";
 import { useFormState, useFormStatus } from "react-dom";
 
@@ -34,12 +36,18 @@ export default function LoginForm() {
 
   return (
     <>
-      <form action={action}>
+      <form action={action} className={styles.form}>
         <Input label="Username" name="username" type='text' />
         <Input label="Password" name="password" type='password'/>
         <ErrorMessage error={state.error} />
         <FormButton />
       </form>
+      <Link className={styles.lost} href="/login/lost-password">Forgot your password?</Link>
+      <div className={styles.signup}>
+        <h2 className={styles.subtitle}>Sign Up</h2>
+        <p>Don&apos;t have an account yet?</p>
+        <Link className='button' href="/login/create-account">Sign Up</Link>
+      </div>
     </>
   )
 }
