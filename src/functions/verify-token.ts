9b0d573b2018a3df32231292c72c01a325c88070
Lucas Ribeiro <lucasribeirodesouza@live.com>
2024-06-02 20:40:02 -0300

    feat: verify jwt token signature

---
+++ src/functions/verify-token.ts
@@ -0,0 +1,14 @@
+import { jwtVerify } from "jose";
+
+export default async function verifyToken(token: string): Promise<boolean> {
+  if (!token) return false;
+  try {
+    // await jwtVerify(token, new TextEncoder().encode('token_secret'), {
+    //   algorithms: ['HS256'],
+    // })
+
+    return true
+  } catch (error) {
+    return false;
+  }
+}
